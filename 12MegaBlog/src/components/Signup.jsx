3c4998a20ff639532a122d170606d6f30e081bf6
Jitin Dhakarwal <101663044+Jitin2002@users.noreply.github.com>
2024-02-28 22:30:56 +0530

    signup

---
+++ 12MegaBlog/src/components/Signup.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import authService from "../appwrite/Auth";
+import { useNavigate,Link } from "react-router-dom";
+import { login } from "../store/authSlice";
+import {Button ,Input ,Logo} from './index'
+function Signup (){
+
+
+    return (
+        <div>Signup</div>
+    )
+
+}
+
+export default Signup
