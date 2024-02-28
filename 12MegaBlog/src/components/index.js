d692866ec42d6a7c6aebd609f7972cba11e15ac0
Jitin Dhakarwal <101663044+Jitin2002@users.noreply.github.com>
2024-02-28 22:30:33 +0530

    Login input and button

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,21 @@
 import Header from "./Header/Header";
 
 import Footer from "./Footer/Footer";
-
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
+import Button from './Button'
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
 export {
     Header,
-    Footer}
+    Footer,
+    Container,
+    Logo,
+    LogoutButton,
+    Button,
+    Input,
+    Select,
+    PostCard
+    }
